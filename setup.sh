1390506b8e50dd71b4a6316de191a6d9f14349f4
Heni Opik <134904900+Heniopik@users.noreply.github.com>
2023-06-03 07:38:13 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,6 +17,7 @@ exit 0
 fi
 mkdir /var/lib/premium-script;
 echo "IP=" >> /var/lib/premium-script/ipvps.conf
+#Install Host
 wget https://raw.githubusercontent.com/Heniopik/premium/main/cf.sh && chmod +x cf.sh && ./cf.sh
 #install ssh ovpn
 wget https://raw.githubusercontent.com/Heniopik/premium/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
@@ -26,25 +27,6 @@ wget https://raw.githubusercontent.com/Heniopik/premium/main/ins-vt.sh && chmod 
 rm -f /root/ssh-vpn.sh
 rm -f /root/ins-vt.sh
 rm -f /root/cf.sh
-cat <<EOF> /etc/systemd/system/autosett.service
-[Unit]
-Description=autosetting
-Documentation=https://wa.me/6285349326511
-
-[Service]
-Type=oneshot
-ExecStart=/bin/bash /etc/set.sh
-RemainAfterExit=yes
-
-[Install]
-WantedBy=multi-user.target
-EOF
-systemctl daemon-reload
-systemctl enable autosett
-wget -O /etc/set.sh "https://raw.githubusercontent.com/Heniopik/premium/main/set.sh"
-chmod +x /etc/set.sh
-history -c
-echo "1.2" > /home/ver
 clear
 echo " "
 echo "Installation has been completed!!"
@@ -55,12 +37,12 @@ echo "--------------------------------------------------------------------------
 echo ""  | tee -a log-install.txt
 echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
-echo "   - Websocket TLS        : 443' | tee -a log-install.txt
+echo "   - Websocket TLS        : 443" | tee -a log-install.txt
 echo "   - Websocket Non Tls  : 8080"  | tee -a log-install.txt
 echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442"  | tee -a log-install.txt
 echo "   - Stunnel4                   : 222, 777"  | tee -a log-install.txt
 echo "   - Dropbear                  : 109, 143"  | tee -a log-install.txt
-echo "   - Squid Proxy              : 3128, 8000 (limit to IP Server)"  | tee -a log-install.txt
+echo "   - Squid Proxy              : 3128, 8000"  | tee -a log-install.txt
 echo "   - Badvpn                    : 7100, 7200, 7300"  | tee -a log-install.txt
 echo "   - Nginx                        : 81"  | tee -a log-install.txt
 echo "   - Trojan                        : 2087"  | tee -a log-install.txt
