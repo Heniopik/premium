e1282cd592c233cde9c72b02fa54a166f053adcd
Heni Opik <134904900+Heniopik@users.noreply.github.com>
2023-06-03 07:50:08 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,12 +17,14 @@ exit 0
 fi
 mkdir /var/lib/premium-script;
 echo "IP=" >> /var/lib/premium-script/ipvps.conf
-#Install Host
+
 wget https://raw.githubusercontent.com/Heniopik/premium/main/cf.sh && chmod +x cf.sh && ./cf.sh
 #install ssh ovpn
-wget https://raw.githubusercontent.com/Heniopik/premium/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/Heniopik/premium/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
+
 #install xray
-wget https://raw.githubusercontent.com/Heniopik/premium/main/ins-vt.sh && chmod +x ins-vt.sh && ./ins-vt.sh
+wget https://raw.githubusercontent.com/Heniopik/premium/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
+
 
 rm -f /root/ssh-vpn.sh
 rm -f /root/ins-vt.sh
@@ -37,8 +39,6 @@ echo "--------------------------------------------------------------------------
 echo ""  | tee -a log-install.txt
 echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
-echo "   - Websocket TLS        : 443" | tee -a log-install.txt
-echo "   - Websocket Non Tls  : 8080"  | tee -a log-install.txt
 echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442"  | tee -a log-install.txt
 echo "   - Stunnel4                   : 222, 777"  | tee -a log-install.txt
 echo "   - Dropbear                  : 109, 143"  | tee -a log-install.txt
