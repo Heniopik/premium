090182794d3142052f93b5bf88b479a46ddc7d99
Heni Opik <134904900+Heniopik@users.noreply.github.com>
2023-06-03 07:23:14 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
-if [ -f "/etc/xray/domain" ]; then
+if [ -f "/etc/v2ray/domain" ]; then
 echo "Script Sukses Terinstall"
 exit 0
 fi
