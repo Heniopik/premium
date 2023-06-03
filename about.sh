981655889952c8748704b5782624f742b6bccb62
Heni Opik <134904900+Heniopik@users.noreply.github.com>
2023-06-03 08:25:02 +0800

    Create about.sh

---
+++ about.sh
@@ -0,0 +1,20 @@
+Ini#!/bin/bash
+
+clear
+echo -e "=================================================" | lolcat
+echo -e "#         NYARI GRATISAN VPN STORE         #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Build Up By AnuyBazoelk                         #"
+echo -e "#-----------------------------------------------#"
+echo -e "# CONTACT PERSON                                #"
+echo -e "#-----------------------------------------------#"
+echo -e "#                                      #"
+echo -e "# https://t.me/anuybazoelk639                                 #"
+echo -e "# https://t.me/anuybazoelk                     #"
+echo -e "# https://wa.me/6285359326511                           #"
+echo -e "# https://wa.me/6287728411949                 #"
+echo -e "#-----------------------------------------------#"
+echo -e "================================================="
