67511f64a1f318fc7146dfb61a39b70d1d4cfad2
Heni Opik <134904900+Heniopik@users.noreply.github.com>
2023-06-03 11:15:11 +0800

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -14,8 +14,6 @@ sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
 cd /root/
 wget -O acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
 bash acme.sh --install >/dev/null 2>&1
-bash acme.sh --register-account -m anuybazoelk639@gmail.com
-wget https://get.acme.sh >/dev/null 2>&1 | sh -s email=anuybazoelk639@gmail.com
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
 /root/.acme.sh/acme.sh --upgrade --auto-upgrade >/dev/null 2>&1
 /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt >/dev/null 2>&1
