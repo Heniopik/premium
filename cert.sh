57038ae63f158ba6a0850184817a86cab43485dd
Heni Opik <134904900+Heniopik@users.noreply.github.com>
2023-06-03 11:44:16 +0800

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#bin/bash
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
@@ -8,15 +8,29 @@ clear
 echo start
 sleep 0.5
 source /var/lib/premium-script/ipvps.conf
-domain="$IP"
+domain="$(cat /etc/xray/domain)"
 sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
 
 cd /root/
 wget -O acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
 bash acme.sh --install >/dev/null 2>&1
-/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+bash acme.sh --register-account -m anuybazoelk639@gmail.com
+wget https://get.acme.sh >/dev/null 2>&1 | sh -s email=anuybazoelk639@gmail.com
 /root/.acme.sh/acme.sh --upgrade --auto-upgrade >/dev/null 2>&1
 /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt >/dev/null 2>&1
-~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+/root/.acme.sh/acme.sh --issue -d $domain --standalone --force --keylength ec-256
+/root/.acme.sh/acme.sh --installcert -d $domain --ecc --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key
+/etc/init.d/ssh restart
+/etc/init.d/dropbear restart
+/etc/init.d/stunnel4 restart
+/etc/init.d/openvpn restart
+/etc/init.d/fail2ban restart
+/etc/init.d/cron restart
+/etc/init.d/nginx restart
+/etc/init.d/squid restart
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
+systemctl restart trojan
 clear
 neofetch
